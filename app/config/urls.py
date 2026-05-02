ae6ddfcea5fc6a9be406d95efe883ba3df0aca71
Prashanti Peña Guevara <shantipa95@gmail.com>
2026-05-02 13:03:03 -0600

    Fix import grouping in urls

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 from django.views.decorators.http import require_GET
-
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
